bb587e65aa6526666018f21480c9bf6dca8d6e11
Adityadarsh81011 <adityai81011@gmail.com>
2020-08-06 00:17:10 +0530

    Updates

---
+++ precache-manifest.cdfdfedc10261b99d567af366b969c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad134473fadea38a8bc530a60844e6c2",
+    "url": "/AdityAdarsh/react-todolist-app/index.html"
+  },
+  {
+    "revision": "69d36457c7f4fddf2145",
+    "url": "/AdityAdarsh/react-todolist-app/static/css/main.3c46908d.chunk.css"
+  },
+  {
+    "revision": "7eb68a875c74e4acaab0",
+    "url": "/AdityAdarsh/react-todolist-app/static/js/2.00a89c2d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AdityAdarsh/react-todolist-app/static/js/2.00a89c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d36457c7f4fddf2145",
+    "url": "/AdityAdarsh/react-todolist-app/static/js/main.b1c6e5fe.chunk.js"
+  },
+  {
+    "revision": "ae57432a417313a7c2f2",
+    "url": "/AdityAdarsh/react-todolist-app/static/js/runtime-main.bd145087.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AdityAdarsh/react-todolist-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
